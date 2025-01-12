59935fe1376148fe15556ba191a11286b10690f8
Hardeep Singh <hardeep.singh@pw.live>
2025-01-13 00:57:47 +0530

    Case 4: should handle an unknown amount of numbers

--- test/src/stringCalculator.test.js
+++ test/src/stringCalculator.test.js
@@ -16,4 +16,9 @@ describe('String Calculator', () => {
     expect(add("3,5")).toBe(8);
   });
 
+  test('should handle an unknown amount of numbers', () => {
+    expect(add("1,2,3")).toBe(6);
+    expect(add("4,5,6,7")).toBe(22);
+  });
+
 });
