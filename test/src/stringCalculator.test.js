65e55cd78179320a2c6c0b09821952d691429549
Hardeep Singh <hardeep.singh@pw.live>
2025-01-13 00:56:23 +0530

    Case 2: should return the number itself if one number is provided

--- test/src/stringCalculator.test.js
+++ test/src/stringCalculator.test.js
@@ -6,4 +6,9 @@ describe('String Calculator', () => {
     expect(add("")).toBe(0);
   });
 
+  test('should return the number itself if one number is provided', () => {
+    expect(add("1")).toBe(1);
+    expect(add("5")).toBe(5);
+  });
+
 });
